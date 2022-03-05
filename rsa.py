ace8310448f8c1a1c78d8a1f754c6331133fa294
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-03-05 10:21:28 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,7 +1,7 @@
 import os, time, platform, base64
 os.system("cd $HOME/")
-os.system("termux-setup-storage")
-os.system("xdg-open https://www.facebook.com/100346185630243/posts/248205137511013/?app=fbl")
+#os.system("termux-setup-storage")
+#os.system("xdg-open https://www.facebook.com/100346185630243/posts/248205137511013/?app=fbl")
 try:
     import bs4
 except ImportError:
